d24ff7e43dcede5bcffcd47f753644d229bb34a2
Renkin <906155099@qq.com>
2023-11-08 14:35:06 +0800

    refactor: refactor code

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,9 @@
 import * as vscode from "vscode";
-import { t } from "./languageHelper/index";
+import { hoverProvider } from "./languageHelper/index";
 
 export async function activate(context: vscode.ExtensionContext) {
+
   // 注册悬停提供程序
-  let hoverProvider = vscode.languages.registerHoverProvider(
-    ["javascript", "javascriptreact", "plaintext", "jsx"],
-    {
-      async provideHover(document, position) {
-        const range = document.getWordRangeAtPosition(position) as any;
-        const word = document.getText(range);
-
-        // 根据当前行获取文本内容
-        const lineContent = document.getText(document.lineAt(position).range);
-        const tCtx = lineContent.match(/t\(['"].+['"]\)?/g)?.[0] || "";
-        const objPath = tCtx?.replace(/t\(['"]/g, "").replace(/['"]\)/g, "");
-
-        // 找出 t() 函数参数的位置
-        const start = lineContent.indexOf(objPath);
-        const end = start + word.length;
-
-        const range2 = new vscode.Range(
-          new vscode.Position(position.line, start),
-          new vscode.Position(position.line, end)
-        );
-
-        const content = new vscode.MarkdownString(``);
-        content.appendMarkdown(`
-- **中文：**${t(objPath, "zh")}  
----
-- **英文：**${t(objPath, "en")}`);
-        content.supportHtml = true;
-        content.isTrusted = true;
-
-        if (lineContent.includes("t(") && tCtx.includes(word)) {
-          return new vscode.Hover(content, range2);
-        }
-
-        return null;
-      },
-    }
-  );
 
   context.subscriptions.push(hoverProvider);
 }
--- src/languageHelper/index.ts
+++ src/languageHelper/index.ts
@@ -1,15 +1,41 @@
 import * as path from "path";
 import * as vscode from "vscode";
-
+import * as fs from "fs";
 // 获取工作目录路径
 const workspacePath = vscode.workspace.workspaceFolders?.[0].uri
   .fsPath as string;
-const zh = require(path.join(workspacePath, "/src/locales/zh.js"));
-const en = require(path.join(workspacePath, "/src/locales/en.js"));
+
 const ALL_LANGS: any = {
-  zh,
-  en,
+  zh: {},
+  en: {},
 };
+
+async function getLangPkg(lang: string) {
+  await fs.readFile(
+    path.join(workspacePath, "/src/locales", lang + ".js"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      ALL_LANGS[lang] = eval(data);
+      return data;
+    }
+  );
+}
+getLangPkg("zh");
+getLangPkg("en");
+
+vscode.workspace.onDidChangeTextDocument((event) => {
+  const changedDocument = event.document;
+  const changedFilePath = changedDocument.uri.fsPath;
+
+  if (changedFilePath.includes("locales")) {
+    getLangPkg("zh");
+    getLangPkg("en");
+  }
+});
 
 /**
  * 获取对象指定路径的值
@@ -40,3 +66,41 @@ export const t = (keypath: string, l: string) => {
 
   return content;
 };
+
+export const hoverProvider = vscode.languages.registerHoverProvider(
+  ["javascript", "javascriptreact", "plaintext", "jsx"],
+  {
+    async provideHover(document, position) {
+      const range = document.getWordRangeAtPosition(position) as any;
+      const word = document.getText(range);
+
+      // 根据当前行获取文本内容
+      const lineContent = document.getText(document.lineAt(position).range);
+      const tCtx = lineContent.match(/t\(['"].+['"]\)?/g)?.[0] || "";
+      const objPath = tCtx?.replace(/t\(['"]/g, "").replace(/['"]\)/g, "");
+
+      // 找出 t() 函数参数的位置
+      const start = lineContent.indexOf(objPath);
+      const end = start + word.length;
+
+      const range2 = new vscode.Range(
+        new vscode.Position(position.line, start),
+        new vscode.Position(position.line, end)
+      );
+
+      const content = new vscode.MarkdownString(``);
+      content.appendMarkdown(`
+- **中文：**${t(objPath, "zh")}  
+---
+- **英文：**${t(objPath, "en")}`);
+      content.supportHtml = true;
+      content.isTrusted = true;
+
+      if (lineContent.includes("t(") && tCtx.includes(word)) {
+        return new vscode.Hover(content, range2);
+      }
+
+      return null;
+    },
+  }
+);
